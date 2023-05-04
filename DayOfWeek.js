2662e87bbda70380c77db7701cec7042982c8a4c
Yoimiya3 <132576431+Yoimiya3@users.noreply.github.com>
2023-05-04 21:50:58 +0800

    Add files via upload

---
+++ DayOfWeek.js
@@ -0,0 +1,38 @@
+function add() {
+    var num1 = parseInt(document.getElementById("num1").value);
+    var num2 = parseInt(document.getElementById("num2").value);
+    var result = num1 + num2;
+    document.getElementById("result").value = result;
+  }
+  
+  function subtract() {
+    var num1 = parseInt(document.getElementById("num1").value);
+    var num2 = parseInt(document.getElementById("num2").value);
+    var result = num1 - num2;
+    document.getElementById("result").value = result;
+  }
+  
+  function multiply() {
+    var num1 = parseInt(document.getElementById("num1").value);
+    var num2 = parseInt(document.getElementById("num2").value);
+    var result = num1 * num2;
+    document.getElementById("result").value = result;
+  }
+  
+  function divide() {
+    var num1 = parseInt(document.getElementById("num1").value);
+    var num2 = parseInt(document.getElementById("num2").value);
+    var result = num1 / num2;
+    document.getElementById("result").value = result;
+  }
+  
+  function clearFields() {
+    document.getElementById("num1").value = "";
+    document.getElementById("num2").value = "";
+    document.getElementById("result").value = "";
+  }
+  
+  function about() {
+    alert("Work of Argel D. Bugarin");
+  }
+  
